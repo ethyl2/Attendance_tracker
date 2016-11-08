63fd5816d147451dd4be38ec3990c2ed14863ab6
Heather Nuffer <zzzuzz@gmail.com>
2016-11-07 22:40:48 -0700

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
-/* STUDENTS IGNORE THIS FUNCTION
- * All this does is create an initial
+/* Creates an initial
  * attendance record if one is not found
  * within localStorage.
  */
@@ -28,6 +27,130 @@
 
 
 /* STUDENT APPLICATION */
+
+//Function for initializing the number of missed days, used in the model.
+var calculateNumMissedDays = function() {
+  var returnArray = [];
+  var attendance = JSON.parse(localStorage.attendance);
+  for (var i = 0; i < Object.keys(attendance).length; i++) {
+    var name = Object.keys(attendance)[i];
+    var record = attendance[name];
+    console.log(record);
+    var numDaysMissed = 0;
+    for (var j = 0; j < record.length; j++) {
+      if (record[j] == false) {
+        numDaysMissed++;
+      }
+    }
+    console.log(name + " missed " + numDaysMissed + " days");
+    //model.numMissedDays[i] = numDaysMissed;
+    returnArray[i] = numDaysMissed;
+  }
+  return returnArray;
+};
+
+// Model
+
+var model = {
+  attendance: JSON.parse(localStorage.attendance),
+  numMissedDays: calculateNumMissedDays()
+};
+
+// Controller
+
+var controller = {
+  //init
+  init: function() {
+    view.init();
+  },
+
+  //return model.attendance
+  getAttendance: function() {
+    return model.attendance;
+  },
+
+  //return model.numMissedDays
+  getNumMissedDays: function() {
+    return model.numMissedDays;
+  },
+
+  //calculate num missed days
+  calculateNumMissedDays: function(newRecord) {
+    for (var i = 0; i < Object.keys(newRecord).length; i++) {
+      var name = Object.keys(newRecord)[i];
+      var record = newRecord[name];
+      console.log(record);
+      var numDaysMissed = 0;
+      for (var j = 0; j < record.length; j++) {
+        if (record[j] == false) {
+          numDaysMissed++;
+        }
+      }
+      console.log(name + " missed " + numDaysMissed + " days");
+      model.numMissedDays[i] = numDaysMissed;
+    }
+    console.log(model.numMissedDays);
+    return model.numMissedDays;
+  },
+
+  //Update the model and view once a click event has happened
+  updateAttendance: function(newRecord) {
+    model.attendance = newRecord;
+    view.init();
+  }
+};
+
+// View
+
+var view = {
+  init: function() {
+
+    // Check boxes, based on attendance records
+    var attendance = controller.getAttendance();
+    $.each(attendance, function(name, days) {
+        var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
+            dayChecks = $(studentRow).children('.attend-col').children('input'),
+            missedCol = $(studentRow).children('.missed-col');
+
+        dayChecks.each(function(i) {
+          //.prop( propertyName, value ) Sets one or more properties for the set of matched elements.
+            $(this).prop('checked', days[i]);
+        });
+    });
+
+    //display num missed days
+    $allMissed = $('tbody .missed-col');
+    var missedDaysArray = controller.getNumMissedDays();
+    $allMissed.each(function(i) {
+      $(this).text(missedDaysArray[i]);
+    })
+
+    //event listener for checking boxes
+    $allCheckboxes = $('tbody input');
+    $allCheckboxes.on('click', function() {
+        var studentRows = $('tbody .student'),
+            newAttendance = {};
+
+        studentRows.each(function() {
+            var name = $(this).children('.name-col').text(),
+                $allCheckboxes = $(this).children('td').children('input');
+
+            newAttendance[name] = [];
+
+            $allCheckboxes.each(function() {
+                newAttendance[name].push($(this).prop('checked'));
+            });
+        });
+
+        controller.calculateNumMissedDays(newAttendance);
+        controller.updateAttendance(newAttendance);
+    });
+  }
+}
+controller.init();
+
+//Below is the original code:
+/*
 $(function() {
     var attendance = JSON.parse(localStorage.attendance),
         $allMissed = $('tbody .missed-col'),
@@ -82,3 +205,4 @@ $(function() {
 
     countMissing();
 }());
+*/
